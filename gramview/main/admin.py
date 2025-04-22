a0310098fccf2ebd17db55830eae3119bcc7d2b0
Kamila Dilmukhametova <alima1k@MacBook-Air--Kamila.local>
2025-04-22 22:07:13 +0300

    db+admin

--- gramview/main/admin.py
+++ gramview/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Service, Review
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    pass
+
+admin.site.register(Service)
+admin.site.register(Review)
--- gramview/main/forms.py
+++ gramview/main/forms.py
@@ -1,11 +1,22 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
 
 class CustomLoginView(LoginView):
     template_name = 'auth/authorization.html'
+
+User = get_user_model()
+
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+            base_group, created = Group.objects.get_or_create(name='base')
+            user.groups.add(base_group)
+        return user
